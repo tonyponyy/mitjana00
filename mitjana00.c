0a45ce39fd7a32175da362f9a775111d6310c71d
unknown <antoniandreut@gmail.com>
2022-05-11 17:55:23 +0200

    Conflicte 1 entre master i br0

--- mitjana00.c
+++ mitjana00.c
@@ -21,6 +21,8 @@ int main() {
 		scanf(" %c",&consulta); 
 	}
 	mitjana=suma/numero_repeticions;
+
 	printf("La mitjana resultant és: %.2f\n",mitjana);	
+	printf("La mitjana ha estat trobada amb èxit\n");
 	return 0;
 }
